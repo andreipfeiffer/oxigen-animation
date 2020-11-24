a7586319063f484442028e9555633051fd1176d6
Andrei Pfeiffer <andrei@upsidedown.ro>
2020-11-24 10:56:16 +0200

    Build package for browser

--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,22 @@
 import typescript from "rollup-plugin-typescript2";
+import resolve from "rollup-plugin-node-resolve";
 import pkg from "./package.json";
 
 export default {
   input: "src/index.ts",
-  output: [
-    {
-      file: pkg.main,
-      format: "cjs",
+  output: {
+    file: "dist/oxygen-animation.js",
+    format: "iife",
+    globals: {
+      animejs: "anime",
     },
-    {
-      file: pkg.module,
-      format: "es",
-    },
-  ],
+  },
   external: [
-    // ...Object.keys(pkg.dependencies || {}),
+    ...Object.keys(pkg.dependencies || {}),
     ...Object.keys(pkg.peerDependencies || {}),
   ],
   plugins: [
+    resolve({ browser: true }),
     typescript({
       typescript: require("typescript"),
     }),
