f633448b17ff91ec6610ecca871422a9af31ce91
Andrei Pfeiffer <andrei@upsidedown.ro>
2020-11-25 16:17:04 +0200

    Handle onchange instead of onsubmit in example

--- docs/script.js
+++ docs/script.js
@@ -2,56 +2,47 @@
 // if you use ES6 modules, you can also use it as:
 // import * as oxygen_animation from "./index";
 
-function init() {
-  const stage = document.querySelector("#animation-stage");
-  const necesar = +init_form.querySelector('input[name="necesar"]').value;
+const stage = document.querySelector("#animation-stage");
 
+const init_form = document.querySelector("#init-form");
+const update_form = document.querySelector("#update-form");
+const animate_form = document.querySelector("#animate-form");
+
+const necesar = init_form.querySelector('input[name="necesar"]');
+const strans = update_form.querySelector('input[name="strans"]');
+const donatori = update_form.querySelector('input[name="donatori"]');
+const nume = animate_form.querySelector('input[name="nume"]');
+const suma = animate_form.querySelector('input[name="suma"]');
+
+function init() {
   oxygen_animation.init({
     element: stage,
-    total_necesar: necesar,
+    total_necesar: +necesar.value,
   });
 }
 
 function update() {
-  const suma = +update_form.querySelector('input[name="suma"]').value;
-  const donatori = +update_form.querySelector('input[name="donatori"]').value;
-
   oxygen_animation.update({
-    donatori,
-    total_strans: suma,
+    donatori: +donatori.value,
+    total_strans: +strans.value,
   });
 }
 
 function animate() {
-  const nume = animate_form.querySelector('input[name="nume"]').value;
-  const suma = +animate_form.querySelector('input[name="suma"]').value;
-
-  oxygen_animation.animate({ nume, suma });
+  oxygen_animation.animate({
+    nume: nume.value,
+    suma: +suma.value,
+  });
 }
 
-const init_form = document.querySelector("#init-form");
+init();
+update();
 
-init_form.addEventListener(
-  "submit",
-  (event) => {
-    event.preventDefault();
-    init();
-  },
-  false
-);
-
-const update_form = document.querySelector("#update-form");
-
-update_form.addEventListener(
-  "submit",
-  (event) => {
-    event.preventDefault();
-    update();
-  },
-  false
-);
-
-const animate_form = document.querySelector("#animate-form");
+necesar.addEventListener("keyup", init, false);
+strans.addEventListener("keyup", update, false);
+donatori.addEventListener("keyup", update, false);
+nume.addEventListener("keyup", animate, false);
+suma.addEventListener("keyup", animate, false);
 
 animate_form.addEventListener(
   "submit",
